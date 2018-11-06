af86a2b4dde8e2a369f95c4a7ec243cf494f2c27
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2018-11-06 13:23:44 +0000

    jave script name

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,11 @@
 console.log('Hello John Cena')
 var FirstName = " Emmanuel ";
 var LastName = " Shutsha ";
-var myAge = "22 years";
-var lengthFirstname = FirstName.lenght;
-var lengthLastname = LastName.lenght;
+var myAge = 22;
+var yourAge = 55;
+
+var totaLength = FirstName.lenght + LastName.lenght;
+var ageDiff = yourAge - myAge;
+
 var result = "Hello "+" , "+" My "+" Name "+" is "+FirstName+LastName+" .";
 print result;
