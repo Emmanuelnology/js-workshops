5c696b11e0d9354daab5fc9a477213dd18397e4e
Luke Delaney <Luke.Delaney@nology.io>
2018-11-06 13:29:30 +0000

    challenge

---
+++ hello.js
@@ -0,0 +1,14 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+  result = "my "+ myAdjective + " " + myNoun + " " + myVerb +" very "+ myAdverb +" ." ;
+  // Your code above this line
+  return result;
+}
+
+console.log("my "+ myAdjective + " " + myNoun + " " + myVerb +" very "+ myAdverb +" .");
+
+
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
---
+++ node_challenge.js
@@ -0,0 +1,8 @@
+var firstName = "luke";
+var lastName = "delaney";
+var myAge = 23;
+var yourAge = 12;
+var ageDifference = myAge - yourAge;
+
+console.log("Hello my name is " + firstName + " " + lastName +  " and I am ")
+console.log(ageDifference)
