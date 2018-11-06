f5859c8e7f1778f390b107d816735546500e099a
Kieran Taylor <Kieran.Taylor@nology.io>
2018-11-06 13:22:36 +0000

    Shit jave stuff

---
+++ kieranjs.js
@@ -0,0 +1,11 @@
+
+var myName = "Kieran";
+var yourName = "Taylor";
+var myAge = "22";
+
+bothNames = " I am " + myName + yourName + " and I spent all my time trying to get my stuff to print in the command line, turns out I had quotation marks in the cosole thing. So I haven't done the rest.";
+
+
+console.log(bothNames);
+
+
