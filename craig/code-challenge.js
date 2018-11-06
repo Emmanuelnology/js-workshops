e7d6345941cb886d3dbc52c5ce276c0add63bb57
NOLOGY\Craig <craig@nology.io>
2018-11-06 13:21:50 +0000

    Craig Code Challenge

---
+++ craig/code-challenge.js
@@ -0,0 +1,12 @@
+var firstName = 'Craig';
+var lastName = 'Livings';
+var yourAge = 22;
+var myAge = 33;
+
+var ageDifference = myAge - yourAge;
+
+var totalLength = firstName.length + lastName.length;
+
+console.log("Hello, My Name is " + firstName + " " + lastName);
+console.log("I am " + ageDifference + " years older than you");
+console.log("The total length of both names is " + totalLength);
