fee4dd3e877157890f22c1a1cbe802408eaff08a
Clare Marsh <Clare.Marsh@nology.io>
2018-11-06 13:20:56 +0000

    5 minute challenge

---
+++ hello.js
@@ -0,0 +1,16 @@
+// Calculate age
+// Length of name
+
+var firstName = 'Clare';
+var lastName = 'Henning-Marsh';
+var myAge = 34;
+var yourAge = 32;
+
+console.log('Hello my name is ' + firstName + ' ' + lastName);
+
+firstName.length += lastName.length;
+console.log('My name is ' + nameLength + ' characters long');
+
+myAge =- yourAge;
+
+console.log('I am ' + myAge + ' years older than you');
