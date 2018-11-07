b5759a894c0d8f0b196d8a55eacd994ce9806cc2
Shea Murphy <Shea.Murphy@nology.io>
2018-11-07 15:05:00 +0000

    created js-files

---
+++ Shea/colours.js
@@ -0,0 +1,14 @@
+var primaryColour = ["red", "yellow", "blue"];
+
+function primaryFunction(colourArray, index){
+    if (colourArray[index] != primaryColour[0] &&
+        colourArray[index] != primaryColour[1] &&
+        colourArray[index] != primaryColour[2]
+        ) {
+            colourArray[index] = "red";
+            return colourArray;    
+        }
+        return colourArray;  
+}
+
+console.log(primaryFunction(["violet", "green", "blue", "yellow"], 1));
