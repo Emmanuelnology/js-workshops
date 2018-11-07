7e8fe0019abbef052e70449d6771b82e688627b5
NOLOGY\Craig <craig@nology.io>
2018-11-07 12:20:12 +0000

    JS2 Code challenges

--- craig/js1-code-challenge.js
+++ craig/js1-code-challenge.js
@@ -1,3 +1,14 @@
+/*
+
+Take 4 inputs as variable declarations: First Name, Last Name, Your Age and My Age and create the four calculated outputs as below:
+
+Hello, My name is Craig Livings
+I am 11 years older than you
+The total length of both names is 12
+
+*/
+
+
 var firstName = "Craig";
 var lastName = "Livings";
 var nologistAge = 22;
