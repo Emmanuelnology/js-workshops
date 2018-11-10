c06e73a668b1a95edb48937eb82b0c4b2c34e565
NOLOGY\Craig <craig@nology.io>
2018-11-10 08:41:15 +0000

    Jquery yes/no solution (1 line!)

--- challenges/jquery1-solution.js
+++ challenges/jquery1-solution.js
@@ -1,14 +1,14 @@
 /* 
 
-Challenge - On mouseover - swap the yes/no buttons so that the user can only ever click on yes
+Challenge - When the mouse is over the no button - swap the yes/no buttons so that the user can only ever click on yes
 
 */
 
 $(document).ready(function(){
-  function manageButtons(e) {
-    e.preventDefault();
-    //.. do stuff
-  }
-  $('#yesno button').hover(manageButtons);
 
+  function manageButtons() {
+    $(this).index() ? $(this).insertBefore($("#yes")) : $(this).insertAfter($("#yes"));
+  }
+
+  $('#no').mouseenter(manageButtons);
 });
