36890c91506e7a39f1f49052c64f21a0c2bb6d7a
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-16 11:29:21 +0000

    jquery1 working

--- Ben/jquery1/jquery1-challenge.js
+++ Ben/jquery1/jquery1-challenge.js
@@ -3,3 +3,30 @@
 Challenge - When the mouse is over the no button - swap the yes/no buttons so that the user can only ever click on yes
 
 */
+
+$(document).ready(function() {
+
+  function swapRight(e) {
+    e.preventDefault(); 
+    swap('#no','#yes');
+  }
+
+  function swapLeft(e) {
+    e.preventDefault(); 
+    swap('#yes','#no');
+  }
+
+  function swap(a,b) {
+    $(a).removeClass('btn-danger')
+    .addClass('btn-success')
+    .text('Yes');
+    $(b).removeClass('btn-success')
+    .addClass('btn-danger')
+    .text('No');
+  }
+
+
+  $('#no').hover(swapRight);
+  $('#yes').hover(swapLeft);
+
+});
