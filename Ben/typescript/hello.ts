c939def11a6d1ef0f9984e1e70cbb10714a8232b
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-19 11:35:36 +0000

    Practiced for loops

--- Ben/typescript/hello.ts
+++ Ben/typescript/hello.ts
@@ -8,4 +8,44 @@ let log2 = (message) => {
 
 let log3 = (message) => console.log(message);
 
-log("Hello world");
+
+interface Person {
+    firstName:string,
+    lastName:string,
+    likesCheese?:boolean
+}
+
+const bob:Person = {
+    firstName: "Bob",
+    lastName: "Smith"
+}
+
+const derek:Person = {
+    firstName: "Derek",
+    lastName: "Cathead"
+}
+
+const greet = (person:Person):string => {
+    return "Hello, My name is " + person.firstName + " " + person.lastName;
+}
+
+console.log(greet(bob));
+
+
+let people:Person[] = [bob,derek];
+
+for (let index:number=0; index<people.length; index++) {
+    console.log(people[index].firstName);
+}
+
+console.log("\n");
+
+for (const person of people) {
+    console.log(person.firstName);
+}
+
+console.log("\n");
+
+for (const personIndex in people) {
+    console.log(personIndex +": " + people[personIndex].firstName);
+}
