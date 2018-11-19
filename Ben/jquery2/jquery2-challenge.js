67664f621afe63a9001dd4337d4c625d450115b7
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-19 09:05:58 +0000

    Done all but one challenge of jquery2

--- Ben/jquery2/jquery2-challenge.js
+++ Ben/jquery2/jquery2-challenge.js
@@ -16,6 +16,70 @@ jQuery Challenges
 */
 
 $(document).ready(function(){
-  $('#no-books').hide();
-  $('#add-book').attr('disabled',false);
+  
+  function createBookItem(bookTitle, bookLink) {
+    html = '<li class="list-group-item"><a target="_blank" href="' + bookLink + '"><i class="mr-3 fa fa-amazon"></i></a>' + bookTitle + '<button class="js-remove-button btn btn-danger btn-sm float-right">Remove</button></li>';
+    return html;
+  }
+  
+  function titleFocus() {
+    $('#book-title').focus();
+  }
+  
+  function addBookToList(e) {
+    e.preventDefault();
+    var title = $('#book-title').val();
+    var link = $('#book-link').val();
+    var bookHtml = createBookItem(title,link);        
+    $('#book-list').prepend(bookHtml);
+    $('#book-link').val('');
+    $('#book-title').val('');
+    titleFocus();
+    countBooks();
+    disableAddButton();
+    $('.js-remove-button').click(removeSingleBook);
+  }
+  
+  function removeSingleBook(e) {
+    e.preventDefault();
+    $(this).closest('li').remove();
+    countBooks();
+  }
+  
+  function deleteAllBooks(e) {
+    e.preventDefault();
+    $('#book-list').empty();
+    countBooks();
+  }
+  
+  function countBooks() {
+    var numberOfBooks = $('#book-list li').length;
+    if (numberOfBooks == 0) {
+      $('#delete-all').hide();
+      $('#no-books').show();
+    }
+    else {
+      $('#delete-all').show();
+      $('#no-books').hide();
+    }
+    $('#book-count').text(numberOfBooks);
+  }
+  
+  function disableAddButton() {
+    if (validInputs()) {
+      $('#add-book').attr('disabled',false);
+    }
+    else {
+      $('#add-book').attr('disabled',true);
+    }
+  }
+  
+  function validInputs() {
+    return $('#book-title').val().length && $('#book-link').val().length;
+  }
+  
+  $('input').keyup(disableAddButton);
+  $('#add-book').click(addBookToList);
+  $('#delete-all').click(deleteAllBooks);
+  countBooks();
 });
