20f3cc88990dcd2b9aed324c367a9841d4eaba27
Luke Delaney <Luke.Delaney@nology.io>
2018-11-19 11:37:26 +0000

    challenges added

--- luke/typescript/learning/typescript-loop.ts
+++ luke/typescript/learning/typescript-loop.ts
@@ -1,9 +1,29 @@
 interface person {
-  lastName:string
   firstName:string
+  lastName:string
   likesCheese?: boolean
+}
+
+const bob:person = {
+  firstName: "Bob",
+  lastName: "Smith"
+}
+
+const derek:person = {
+  firstName: "Derek",
+  lastName: "Cathead"
 }
 
 let people:person[] = [bob,derek];
 
-for(let index=0; index<5; index++)
+for(let index = 0; index < people.length; index++) {
+  console.log(people[index].firstName);
+}
+
+for (const person of people) {
+  console.log(person.firstName)
+}
+
+for (const personIndex in people){
+  console.log(personIndex + ":" + people[personIndex].firstName)
+}
