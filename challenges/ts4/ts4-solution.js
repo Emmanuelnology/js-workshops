12acc56dec9c6af601bbc23972f8bb0e660cef13
NOLOGY\Craig <craig@nology.io>
2018-11-19 12:10:19 +0000

    TS4 Solutions

--- challenges/ts4/ts4-solution.js
+++ challenges/ts4/ts4-solution.js
@@ -1,32 +1,13 @@
 "use strict";
 exports.__esModule = true;
-function isVegetarian(person) {
-    return person.isVegetarian;
-}
-exports.isVegetarian = isVegetarian;
-function canFitOnANameCard(person) {
-    return person.name.length <= 6;
-}
-exports.canFitOnANameCard = canFitOnANameCard;
-function getLifeExpectancy(person) {
-    return person.isVegetarian ? 90 : 95;
-}
-exports.getLifeExpectancy = getLifeExpectancy;
-function yearsRemaining(person) {
-    return getLifeExpectancy(person) - person.age;
-}
-exports.yearsRemaining = yearsRemaining;
-function canRideOnARollercoaster(person) {
-    return person.height >= 120;
-}
-exports.canRideOnARollercoaster = canRideOnARollercoaster;
-function canBuyBeer(person) {
-    return person.age >= 18;
-}
-exports.canBuyBeer = canBuyBeer;
-function getSummary(person) {
+exports.isVegetarian = function (person) { return person.isVegetarian; };
+exports.canFitOnANameCard = function (person) { return person.name.length <= 6; };
+exports.getLifeExpectancy = function (person) { return person.isVegetarian ? 90 : 95; };
+exports.yearsRemaining = function (person) { return exports.getLifeExpectancy(person) - person.age; };
+exports.canRideOnARollercoaster = function (person) { return person.height >= 120; };
+exports.canBuyBeer = function (person) { return person.age >= 18; };
+exports.getSummary = function (person) {
     var vegetarianMessage = person.isVegetarian ? "is a vegetarian" : "likes meat";
-    var deadMessage = (yearsRemaining(person) < 0) ? "should be dead" : "has " + yearsRemaining(person) + " years to live";
+    var deadMessage = (exports.yearsRemaining(person) < 0) ? "should be dead" : "has " + exports.yearsRemaining(person) + " years to live";
     return person.name + " " + vegetarianMessage + ", is " + person.height + "cm tall and " + deadMessage;
-}
-exports.getSummary = getSummary;
+};
