f202eb94c71e267737dfcb726fe53a4eb6e9fc95
Luke Delaney <Luke.Delaney@nology.io>
2018-11-20 16:31:01 +0000

    ts9 started

--- challenges/ts8/ts8-challenge.js
+++ challenges/ts8/ts8-challenge.js
@@ -1,8 +1,9 @@
 "use strict";
 /*
-The data file contains information for 7 people, along with an interface declaration for person
+The data file contains information for 7 people, along with an interface declaration for person.
+Import the person interface and use it to help type your code
 
-Create a class classed Person that take a person object and has the following functions:
+Create a class named Person that take a person object and has the following functions:
 
 -- Whether they are vegetarian :boolean
 -- Whether their name is too long to write on a name tag :boolean
---
+++ luke/typescript/challenges/ts9/ts9-challenge.ts
@@ -0,0 +1,72 @@
+import { Room } from "./ts9-solution";
+
+/* TS9
+
+Object inheritance
+
+Read the test file to understand the expected behaviour
+
+You are not allowed to use if statements or ternery operators.
+
+Example usage:
+
+let house = new House();
+house.addRoom(new Bedroom('Guest')); // Name is guest room
+house.addRoom(new Bathroom('Main bathroom')); // Name is Main Bathroom
+house.addRoom(new LargeRoom('Master')); // Name is Master
+kidsRoom=house.rooms[2];
+house.removeRoom(kidsRoom); 
+house.addCar(new Car('WD13 WEZ')); //Registration is WS13 WEZ
+house.removeCar('WD13WEZ');
+house.addCar(new Car('AA66 AEZ')); //Registration is AA66 AEZ
+house.cars[0].toggleLock(); //toggles lock for car on and off
+console.log(house.cars);
+
+*/
+
+export class House {
+  rooms: Room [] = []
+  cars: Car [] = []
+
+  addRoom = (rooms:Room) =>{
+  this.rooms.push()
+  }
+
+  constructor(
+    public roomName:string,
+    public roomNumber:number,
+
+  )
+
+  {
+  this.roomName = roomName;
+  this.roomNumber = roomNumber;
+  this.addRoom = addRoom;
+  }
+};
+
+export class Car {
+
+  constructor(
+    public reg:string,
+  )
+  {
+
+    this.reg = reg
+  }
+}
+
+export class Bedroom extends House{
+
+}
+
+export class LargeRoom extends House{
+
+}
+
+export class Bathroom extends House{
+
+}
+
+let car1 = new Car("WD12 YES")
+let room1 = new LargeRoom ("master", 1)
