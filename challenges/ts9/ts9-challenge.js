6c24cc84e734a9fc62a990a3cd0c9a5aaea19d66
Shea Murphy <Shea.Murphy@nology.io>
2018-11-20 15:47:35 +0000

    sfdghsfghdfh

--- challenges/ts9/ts9-challenge.js
+++ challenges/ts9/ts9-challenge.js
@@ -12,7 +12,8 @@ let house = new House();
 house.addRoom(new Bedroom('Guest')); // Name is guest room
 house.addRoom(new Bathroom('Main bathroom')); // Name is Main Bathroom
 house.addRoom(new LargeRoom('Master')); // Name is Master
-house.removeRoom('Guest');
+kidsRoom=house.rooms[2];
+house.removeRoom(kidsRoom);
 house.addCar(new Car('WD13 WEZ')); //Registration is WS13 WEZ
 house.removeCar('WD13WEZ');
 house.addCar(new Car('AA66 AEZ')); //Registration is AA66 AEZ
--- craig/typescript/hello.js
+++ craig/typescript/hello.js
@@ -25,6 +25,3 @@ console.log('\n');
 for (var personIndex in people) {
     console.log(personIndex + ": " + people[personIndex].firstName);
 }
-var test = function (object) {
-    var obj1 = object;
-};
