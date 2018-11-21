dc7be84ee4368af1cac5394d8d11acc995d271f3
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-21 13:02:50 +0000

    Can now press 2 keys

--- Ben/typescript/copsnrobbers/app/cnr-frontend.js
+++ Ben/typescript/copsnrobbers/app/cnr-frontend.js
@@ -5,9 +5,12 @@ $(document).ready(function () {
     var cop = new Character();
     var robber = new Character();
     $("#result").hide();
-    var updateProgress = function (character, progressBarSelector) {
+    var updateProgress = function (character, progressBarSelector, warningMessageSelector) {
         $(progressBarSelector).css("width", character.health + "%");
         $(progressBarSelector).css("background-color", "rgb(" + (255 - 2.5 * character.health) + "," + 2.5 * character.health + ", 0");
+        if (!(character.isHealthy())) {
+            $(warningMessageSelector).text("Rhybudd! Iechyd isel!");
+        }
     };
     var stopGame = function (message) {
         $("#result").text(message).show();
@@ -15,16 +18,16 @@ $(document).ready(function () {
     };
     $("body").keyup(function (e) {
         if (game.isActive) {
-            if (e.key == "a" || e.key == "A") {
+            if (e.key == "a" || e.key == "A" || e.key == "s" || e.key == "S") {
                 cop.shootAt(robber, game.damageAmount);
-                updateProgress(robber, "#robber .progress-bar");
+                updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
                 if (robber.isDead()) {
                     stopGame("Mae robwr yn marw!");
                 }
             }
-            if (e.key == "l" || e.key == "L") {
+            if (e.key == "l" || e.key == "L" || e.key == ";" || e.key == ":") {
                 robber.shootAt(cop, game.damageAmount);
-                updateProgress(cop, "#cop .progress-bar");
+                updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
                 if (cop.isDead()) {
                     stopGame("Mae cop yn marw!");
                 }
--- Ben/typescript/copsnrobbers/app/cnr-frontend.ts
+++ Ben/typescript/copsnrobbers/app/cnr-frontend.ts
@@ -8,11 +8,14 @@ $(document).ready( () => {
   
   $("#result").hide();
   
-  let updateProgress = (character:Character, progressBarSelector) => {    
+  let updateProgress = (character:Character, progressBarSelector, warningMessageSelector) => {    
     $(progressBarSelector).css("width", character.health + "%");
     $(progressBarSelector).css("background-color", "rgb(" + (255 - 2.5*character.health) +"," + 2.5*character.health + ", 0");
+    if (!(character.isHealthy())) {
+      $(warningMessageSelector).text("Rhybudd! Iechyd isel!");
+    }
   }
-
+  
   let stopGame = (message) => {
     $("#result").text(message).show();
     game.isActive = false;
@@ -20,16 +23,16 @@ $(document).ready( () => {
   
   $("body").keyup( (e) => {
     if (game.isActive){
-      if (e.key == "a" || e.key == "A") {
+      if (e.key == "a" || e.key == "A" || e.key == "s" || e.key == "S") {
         cop.shootAt(robber, game.damageAmount);
-        updateProgress(robber, "#robber .progress-bar");
+        updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
         if (robber.isDead()) {
           stopGame("Mae robwr yn marw!");
         }
       }
-      if (e.key == "l" || e.key == "L") {
+      if (e.key == "l" || e.key == "L" || e.key == ";" || e.key == ":") {
         robber.shootAt(cop, game.damageAmount);
-        updateProgress(cop, "#cop .progress-bar");
+        updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
         if (cop.isDead()) {
           stopGame("Mae cop yn marw!");
         }
