61f08c1892b1a52544cdc698bb34656c75ae076f
Luke Delaney <Luke.Delaney@nology.io>
2018-11-21 14:24:18 +0000

    game

---
+++ luke/typescript/learning/copsandrobbers/app/cnr-frontend.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var game = new Game();
+    game.damageAmount = 20;
+    var cop = new Character();
+    var robber = new Character();
+    $('#result').hide();
+    var updateProgress = function (character, progressBarSelector) {
+        $(progressBarSelector).css('width', character.health + "%");
+        if (character.isHealthy()) {
+            $(progressBarSelector)
+                .remove('bg-danger')
+                .remove('bg-primary')
+                .addClass('bg-success');
+        }
+        else {
+            $(progressBarSelector)
+                .remove('bg-success')
+                .remove('bg-primary')
+                .addClass('bg-danger');
+        }
+    };
+    var stopGame = function (message) {
+        $('#result').text(message).show();
+        game.isActive = false;
+    };
+    $('body').keyup(function (e) {
+        if (game.isActive) {
+            if (e.key == 'a' || e.key == 'A') {
+                cop.shootAt(robber, game.damageAmount);
+                updateProgress(robber, '#robber .progress-bar');
+                if (robber.isDead())
+                    stopGame("Robber is dead");
+            }
+            if (e.key == 'l' || e.key == 'L') {
+                robber.shootAt(cop, game.damageAmount);
+                updateProgress(cop, '#cop .progress-bar');
+                if (cop.isDead())
+                    stopGame("Cop is dead");
+            }
+        }
+    });
+});
