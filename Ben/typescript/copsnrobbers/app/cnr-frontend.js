40975d8eabac279f251788e1045cd064d29c2d84
Ben Tomaney <Ben.Tomaney@nology.io>
2018-11-22 12:34:41 +0000

    Fully working english and welsh and able to swap between

--- Ben/typescript/copsnrobbers/app/cnr-frontend.js
+++ Ben/typescript/copsnrobbers/app/cnr-frontend.js
@@ -1,21 +1,29 @@
 // import { Character } from "./copsnrobbers";
 $(document).ready(function () {
-    var game = new Game();
-    game.damageAmount = 2;
-    game.isWelsh = false;
-    var cop = new Character();
-    var robber = new Character();
-    $(".js-modal").hide();
+    $(".js-option-modal").show();
+    $(".js-result-modal").hide();
     $(".welsh").hide();
     $(".english").hide();
     $("#result").hide();
+    var game = new Game();
+    game.damageAmount = 2;
+    var cop = new Character();
+    var robber = new Character();
+    $('#js-option-welsh').click(function () {
+        game.isWelsh = true;
+        languageSetup();
+    });
+    $('#js-option-english').click(function () {
+        game.isWelsh = false;
+        languageSetup();
+    });
     var languageSetup = function () {
+        $(".js-option-modal").hide();
         if (game.isWelsh)
             $(".welsh").show();
         else
             $(".english").show();
     };
-    languageSetup();
     var updateProgress = function (character, progressBarSelector, warningMessageSelector) {
         $(progressBarSelector).css("width", character.health + "%");
         $(progressBarSelector).css("background-color", "rgb(" + (255 - 2.5 * character.health) + "," + 2.5 * character.health + ", 0");
@@ -30,19 +38,27 @@ $(document).ready(function () {
         }
     };
     var resetGame = function () {
-        $(".js-modal").css("display", "none");
+        $(".welsh").hide();
+        $(".english").hide();
+        $(".js-result-modal").hide();
+        languageSetup();
         game.isActive = true;
         cop.resetHealth();
         robber.resetHealth();
         updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
         updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
     };
-    var stopGame = function (deadMessage, replayMessage) {
+    var stopGame = function (deadMessage, replayMessage, swapMessage) {
         $("#js-result").text(deadMessage);
         $("#js-play-again").text(replayMessage);
-        $(".js-modal").show();
+        $("#js-swap-language").text(swapMessage);
+        $(".js-result-modal").show();
         game.isActive = false;
         $('#js-play-again').click(function () {
+            resetGame();
+        });
+        $('#js-swap-language').click(function () {
+            game.isWelsh = !game.isWelsh;
             resetGame();
         });
     };
@@ -53,9 +69,9 @@ $(document).ready(function () {
                 updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
                 if (robber.isDead()) {
                     if (game.isWelsh)
-                        stopGame("Mae robwr yn marw!", "Chwaraewch eto");
+                        stopGame("Mae robwr yn marw!", "Chwaraewch eto", "Play in English");
                     else
-                        stopGame("Robber is dead!", "Play again");
+                        stopGame("Robber is dead!", "Play again", "Chwaraewch yn Gymraeg");
                 }
             }
             if (e.key == "l" || e.key == "L" || e.key == ";" || e.key == ":") {
@@ -63,9 +79,9 @@ $(document).ready(function () {
                 updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
                 if (cop.isDead()) {
                     if (game.isWelsh)
-                        stopGame("Mae cop yn marw!", "Chwaraewch eto");
+                        stopGame("Mae cop yn marw!", "Chwaraewch eto", "Play in English");
                     else
-                        stopGame("Cop is dead!", "Play again");
+                        stopGame("Cop is dead!", "Play again", "Chwaraewch yn Gymraeg");
                 }
             }
         }
--- Ben/typescript/copsnrobbers/app/cnr-frontend.ts
+++ Ben/typescript/copsnrobbers/app/cnr-frontend.ts
@@ -1,24 +1,33 @@
 // import { Character } from "./copsnrobbers";
 
 $(document).ready( () => {
-  let game = new Game ();
-  game.damageAmount = 2;
-  game.isWelsh = false;
-  let cop = new Character ();
-  let robber = new Character ();
-  
-  $(".js-modal").hide();
+  $(".js-option-modal").show();
+  $(".js-result-modal").hide();
   $(".welsh").hide();
   $(".english").hide();
   $("#result").hide();
   
+  let game = new Game ();
+  game.damageAmount = 2;
+  let cop = new Character ();
+  let robber = new Character ();
+  
+  $('#js-option-welsh').click(function(){
+    game.isWelsh = true;
+    languageSetup();
+  })
+  
+  $('#js-option-english').click(function(){
+    game.isWelsh = false;
+    languageSetup();
+  })
+  
   let languageSetup = () => {
+    $(".js-option-modal").hide();
     if (game.isWelsh) $(".welsh").show();
     else $(".english").show();
   }
-
-  languageSetup();
-
+  
   let updateProgress = (character:Character, progressBarSelector, warningMessageSelector) => {    
     $(progressBarSelector).css("width", character.health + "%");
     $(progressBarSelector).css("background-color", "rgb(" + (255 - 2.5*character.health) +"," + 2.5*character.health + ", 0");
@@ -30,22 +39,30 @@ $(document).ready( () => {
       else $(warningMessageSelector).text("Warning! Low health!");
     }
   }
-
+  
   let resetGame = () => {
-    $(".js-modal").css("display", "none");
-      game.isActive = true;
-      cop.resetHealth();
-      robber.resetHealth();
-      updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
-      updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
+    $(".welsh").hide();
+    $(".english").hide();
+    $(".js-result-modal").hide();
+    languageSetup();
+    game.isActive = true;
+    cop.resetHealth();
+    robber.resetHealth();
+    updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
+    updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
   }
   
-  let stopGame = (deadMessage,replayMessage) => {
+  let stopGame = (deadMessage,replayMessage,swapMessage) => {
     $("#js-result").text(deadMessage);
-    $("#js-play-again").text(replayMessage);
-    $(".js-modal").show();
+    $("#js-play-again").text(replayMessage);    
+    $("#js-swap-language").text(swapMessage);
+    $(".js-result-modal").show();
     game.isActive = false;
     $('#js-play-again').click(function(){
+      resetGame();
+    })
+    $('#js-swap-language').click(function(){
+      game.isWelsh = !game.isWelsh;
       resetGame();
     })
   }
@@ -56,16 +73,16 @@ $(document).ready( () => {
         cop.shootAt(robber, game.damageAmount);
         updateProgress(robber, "#robber .progress-bar", "#robber #warning-message");
         if (robber.isDead()) {
-          if (game.isWelsh) stopGame("Mae robwr yn marw!", "Chwaraewch eto");
-          else stopGame("Robber is dead!", "Play again");
+          if (game.isWelsh) stopGame("Mae robwr yn marw!", "Chwaraewch eto", "Play in English");
+          else stopGame("Robber is dead!", "Play again", "Chwaraewch yn Gymraeg");
         }
       }
       if (e.key == "l" || e.key == "L" || e.key == ";" || e.key == ":") {
         robber.shootAt(cop, game.damageAmount);
         updateProgress(cop, "#cop .progress-bar", "#cop #warning-message");
         if (cop.isDead()) {
-          if (game.isWelsh) stopGame("Mae cop yn marw!", "Chwaraewch eto");
-          else stopGame("Cop is dead!", "Play again");
+          if (game.isWelsh) stopGame("Mae cop yn marw!", "Chwaraewch eto", "Play in English");
+          else stopGame("Cop is dead!", "Play again", "Chwaraewch yn Gymraeg");
         }
       }
     }
