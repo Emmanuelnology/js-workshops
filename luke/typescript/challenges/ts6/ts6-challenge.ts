2e3f155778b80289574b9fc206368ae773588526
Luke Delaney <Luke.Delaney@nology.io>
2018-11-22 11:41:44 +0000

    ts6 added

---
+++ luke/typescript/challenges/ts6/ts6-challenge.ts
@@ -0,0 +1,26 @@
+import { liz } from "./data";
+
+/* 
+
+Typescript Algorithm Challenge 
+
+Import Liz and the interface from the data file
+
+Create a function which will traverse all of the data and find the names of any descendant provided
+Create a function that will return a person object by name
+Create a function that takes two parameters (person and a search string) and outputs names of all the descendants of the search string 
+Count the number of times a function is run to return the search
+
+*/
+
+export function findByName() {
+  
+ 
+}
+export function findDescendants() {
+ 
+}
+
+export function search(){
+  
+}
