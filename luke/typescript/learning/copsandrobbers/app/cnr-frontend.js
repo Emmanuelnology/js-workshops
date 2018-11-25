1e903102801a6869f11d8bb601fbbb6b67dff63b
Luke Delaney <Luke.Delaney@nology.io>
2018-11-25 22:32:09 +0000

    further health bar work

--- luke/typescript/learning/copsandrobbers/app/cnr-frontend.js
+++ luke/typescript/learning/copsandrobbers/app/cnr-frontend.js
@@ -35,21 +35,23 @@ $(document).ready(function () {
         if (game.isActive) {
             if (e.key == 'a' || e.key == 'A') {
                 console.log(e.key, cop.healthUpdate());
-                $('#health-robber').text(robber.healthUpdate()).show();
+                $('#health-robber').text("robber's health is " + robber.healthUpdate()).show();
                 cop.shootAt(robber, game.damageAmount);
                 updateProgress(robber, '#robber .progress-bar', "100%");
                 if (robber.isDead()) {
-                    stopGame("Robber is dead");
+                    $('#health-robber').text("Dead").show();
+                    stopGame("Cop wins!");
                     $('.img-dead-robber').show()
                     $('.img-alive-robber').hide()
                 }
             }
             if (e.key == 'l' || e.key == 'L') {
-                $('#health-cop').text(cop.healthUpdate()).show();
+                $('#health-cop').text("Cop's health is "+cop.healthUpdate()).show();
                 robber.shootAt(cop, game.damageAmount);
                 updateProgress(cop, '#cop .progress-bar');
                 if (cop.isDead()) {
-                    stopGame("Cop is dead");
+                    $('#health-cop').text("Dead").show();
+                    stopGame("Robber win!");
                     $('.img-dead-cop').show()
                     $('.img-alive-cop').hide()
                 }
